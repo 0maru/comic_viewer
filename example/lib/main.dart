9c0f60291d0adecea5df6ca775c27dc022974dd9
Toi Yoshimaru <0maru.dev@gmail.com>
2023-11-11 22:51:47 +0900

    create example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:comic_viewer/comic_viewer.dart';
 import 'package:flutter/material.dart';
 
+const imagePaths = [
+  'assets/image01.png',
+  'assets/image02.png',
+  'assets/image03.png',
+  'assets/image01.png',
+  'assets/image02.png',
+  'assets/image03.png',
+  'assets/image01.png',
+  'assets/image02.png',
+  'assets/image03.png',
+  'assets/image01.png',
+  'assets/image02.png',
+  'assets/image03.png',
+  'assets/image01.png',
+  'assets/image02.png',
+  'assets/image03.png',
+];
+
 void main() {
   runApp(const MyApp());
 }
@@ -66,7 +84,23 @@ class ViewerPage extends StatelessWidget {
         toolBarBackgroundColor: Colors.black,
         bottomBarBackgroundColor: Colors.black,
       ),
-      pageCount: 300,
+      pageCount: imagePaths.length,
+      itemBuilder: (ctx, idx) {
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+          ),
+          constraints: BoxConstraints(
+            minWidth: MediaQuery.of(context).size.width,
+            maxHeight: MediaQuery.of(context).size.width,
+          ),
+          child: Image.asset(
+            imagePaths[idx],
+            fit: BoxFit.contain,
+          ),
+        );
+      },
     );
   }
 }
