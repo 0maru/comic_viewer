069d95a266cd9546c55121459acc507bc0ad4a94
Toi Yoshimaru <0maru.dev@gmail.com>
2023-11-14 01:24:41 +0900

    feat: double tap scale

--- lib/src/page_view.dart
+++ lib/src/page_view.dart
@@ -34,10 +34,30 @@ class _CustomPageViewState extends State<CustomPageView> {
   ///
   int _lastReportedPage = 0;
 
+  ///
+  bool isZoomIn = false;
+
+  late TransformationController _transformationController;
+
   @override
   void initState() {
     super.initState();
     _lastReportedPage = widget.controller.initialPage;
+    _transformationController = TransformationController()
+      ..addListener(() {
+        final scale = _transformationController.value.getMaxScaleOnAxis();
+        if (scale > 1.0 != isZoomIn) {
+          setState(() {
+            isZoomIn = scale > 1.0;
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _transformationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -60,9 +80,11 @@ class _CustomPageViewState extends State<CustomPageView> {
       child: Scrollable(
         axisDirection: axisDirection,
         controller: widget.controller,
-        physics: isVertical
-            ? const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics())
-            : const PageScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        physics: isZoomIn
+            ? const NeverScrollableScrollPhysics()
+            : isVertical
+                ? const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics())
+                : const PageScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
         scrollBehavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
         viewportBuilder: (context, position) {
           return Viewport(
@@ -71,7 +93,29 @@ class _CustomPageViewState extends State<CustomPageView> {
             slivers: [
               SliverFillViewport(
                 delegate: SliverChildBuilderDelegate(
-                  widget.builder,
+                  (ctx, idx) {
+                    return GestureDetector(
+                      onDoubleTapDown: (details) {
+                        if (isZoomIn) {
+                          _transformationController.value = Matrix4.identity();
+                          return;
+                        }
+                        const zoomScale = 3.0;
+                        _transformationController.value = Matrix4.identity()
+                          ..translate(
+                            -(details.localPosition.dx * (zoomScale - 1)),
+                            -(details.localPosition.dy * (zoomScale - 1)),
+                          )
+                          ..scale(zoomScale);
+                      },
+                      child: InteractiveViewer(
+                        transformationController: _transformationController,
+                        minScale: 1,
+                        maxScale: 3,
+                        child: widget.builder(ctx, idx),
+                      ),
+                    );
+                  },
                   childCount: widget.itemCount,
                 ),
               ),
